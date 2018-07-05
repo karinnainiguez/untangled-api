742e6716bbf7dd1ac85c98cdc474d40780aa69de
Karinna Iniguez <karinnainiguez@gmail.com>
2018-07-04 19:13:52 -0700

    Add method to controller for /paths get request

--- src/main/java/com/untangled/api/path/PathController.java
+++ src/main/java/com/untangled/api/path/PathController.java
@@ -1,9 +1,14 @@
 package com.untangled.api.path;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PathController {
 	
+	@RequestMapping("/paths")
+	public String getPaths() {
+		return "All possible paths";
+	}
 
 }
