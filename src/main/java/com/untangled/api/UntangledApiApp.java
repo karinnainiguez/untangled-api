30c872451ce082b065bc95e1532f84861bb302c6
Karinna Iniguez <karinnainiguez@gmail.com>
2018-07-04 18:37:50 -0700

    Add main app class with one method and boot annotations

---
+++ src/main/java/com/untangled/api/UntangledApiApp.java
@@ -0,0 +1,15 @@
+package com.untangled.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UntangledApiApp {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(UntangledApiApp.class, args);
+
+	}
+
+}
