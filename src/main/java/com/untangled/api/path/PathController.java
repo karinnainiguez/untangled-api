9174fda3fbf3e537168533d99850934f41195e3a
Karinna Iniguez <karinnainiguez@gmail.com>
2018-07-07 20:43:22 -0700

    testing api in path controller

--- src/main/java/com/untangled/api/path/PathController.java
+++ src/main/java/com/untangled/api/path/PathController.java
@@ -2,20 +2,23 @@ package com.untangled.api.path;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Hashtable;
 import java.util.List;
 
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class PathController {
+	RestTemplate restTemplate = new RestTemplate();
 	
-	@RequestMapping(
-			value="/paths/{start}/{end}", 
-			method = RequestMethod.GET
-			)
+	@RequestMapping("/paths/{start}/{end}")
 	public List<Path> getPaths(@PathVariable String start, @PathVariable String end) {
 		// create list of topics to return 
 		return Arrays.asList(
@@ -23,6 +26,32 @@ public class PathController {
 				new Path(4, new ArrayList<String>(Arrays.asList("second path url1", "secondpath url2"))), 
 				new Path(5, new ArrayList<String>(Arrays.asList("third path only has one")))
 				);
+	}
+	
+	@RequestMapping("/testapi")
+	public String getResponse() {
+		HttpHeaders headers = new HttpHeaders();
+		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+		HttpEntity<String> entity = new HttpEntity<String>(headers);
+		return restTemplate.exchange(
+				"https://en.wikipedia.org/w/api.php?action=query&format=json&prop=links&pllimit=max&plnamespace=0&titles=Software_development", 
+				HttpMethod.GET, 
+				entity, 
+				String.class
+				).getBody();
+	}
+	
+	@RequestMapping("/newTest")
+	public String newResponse() {
+		HttpHeaders headers = new HttpHeaders();
+		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+		HttpEntity<Hashtable> entity = new HttpEntity<Hashtable>(headers);
+		return restTemplate.exchange(
+				"https://en.wikipedia.org/w/api.php?action=query&format=json&prop=links&pllimit=max&plnamespace=0&titles=Software_development", 
+				HttpMethod.GET, 
+				entity, 
+				String.class
+				).getBody();
 	}
 
 }
