83ffbf164a761c710361ba634999244195f0e05b
Karinna Iniguez <karinnainiguez@gmail.com>
2018-07-09 14:26:41 -0700

    refactor.  move remaining business logic to service for geAllPaths method

--- src/main/java/com/untangled/api/path/PathController.java
+++ src/main/java/com/untangled/api/path/PathController.java
@@ -1,23 +1,12 @@
 package com.untangled.api.path;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Hashtable;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.untangled.api.page.Page;
 
 
@@ -26,30 +15,24 @@ public class PathController {
 	
 	@Autowired
 	private PathService pathService;
-	RestTemplate restTemplate = new RestTemplate();
 	
 	@RequestMapping("/paths/{start}/{end}")
 	public List<Path> getPaths(@PathVariable String start, @PathVariable String end) {
-		// create list of topics to return 
-		return Arrays.asList(
-				new Path(1, new ArrayList<String>(Arrays.asList("thisis my first url string", "this is my second url string", "put in from controller"))), 
-				new Path(4, new ArrayList<String>(Arrays.asList("second path url1", "secondpath url2"))), 
-				new Path(5, new ArrayList<String>(Arrays.asList("third path only has one")))
-				);
+		return pathService.getAllPaths();
 	}
 	
-	@RequestMapping("/testapi")
-	public String getResponse() {
-		HttpHeaders headers = new HttpHeaders();
-		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-		HttpEntity<String> entity = new HttpEntity<String>(headers);
-		return restTemplate.exchange(
-				"https://en.wikipedia.org/w/api.php?action=query&format=json&prop=links&pllimit=max&plnamespace=0&titles=Software_development", 
-				HttpMethod.GET, 
-				entity, 
-				String.class
-				).getBody();
-	}
+//	@RequestMapping("/testapi")
+//	public String getResponse() {
+//		HttpHeaders headers = new HttpHeaders();
+//		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+//		HttpEntity<String> entity = new HttpEntity<String>(headers);
+//		return restTemplate.exchange(
+//				"https://en.wikipedia.org/w/api.php?action=query&format=json&prop=links&pllimit=max&plnamespace=0&titles=Software_development", 
+//				HttpMethod.GET, 
+//				entity, 
+//				String.class
+//				).getBody();
+//	}
 	
 	@RequestMapping("/newTest")
 	public Page newResponse() {
--- src/main/java/com/untangled/api/path/PathService.java
+++ src/main/java/com/untangled/api/path/PathService.java
@@ -1,6 +1,9 @@
 package com.untangled.api.path;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -13,6 +16,15 @@ public class PathService {
 	
 	RestTemplate restTemplate = new RestTemplate();
 	
+	public List<Path> getAllPaths(){
+		// create list of topics to return 
+		return Arrays.asList(
+				new Path(1, new ArrayList<String>(Arrays.asList("thisis my first url string", "this is my second url string", "put in from controller"))), 
+				new Path(4, new ArrayList<String>(Arrays.asList("second path url1", "secondpath url2"))), 
+				new Path(5, new ArrayList<String>(Arrays.asList("third path only has one")))
+				);
+	}
+
 	public Page generatePage() {
 		String stringResponse = restTemplate.getForObject(
 				"https://en.wikipedia.org/w/api.php?action=query&format=json&prop=links&pllimit=max&plnamespace=0&titles=Software_development", 
