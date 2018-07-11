8fda483cc491781e7eef6fed6ab4ab927c46ffd6
Karinna Iniguez <karinnainiguez@gmail.com>
2018-07-11 14:15:52 -0700

    Create empty method where graph library can be used

--- src/main/java/com/untangled/api/path/PathService.java
+++ src/main/java/com/untangled/api/path/PathService.java
@@ -26,17 +26,19 @@ public class PathService {
 	}
 
 	public Page generatePage(String pageName) {
+		// format page name withoug whitespace
 		String formatName = pageName.replaceAll(" ", "_");
+		
+		// get response from external API as string
 		String stringResponse = restTemplate.getForObject(
 				"https://en.wikipedia.org/w/api.php?action=query&format=json&prop=links&pllimit=max&plnamespace=0&titles=" + formatName, 
 				String.class
 				);
-		System.out.println("the ENTIRE response: ");
-		System.out.println(stringResponse);
-		System.out.println("\n\nMe trying to get into the pages:");
-		String subString = stringResponse.substring(stringResponse.indexOf("pageid") - 2, stringResponse.lastIndexOf("]") + 2);
-		System.out.println(subString);
 		
+		// truncate string to only the page info.
+		String subString = stringResponse.substring(stringResponse.indexOf("pageid") - 2, stringResponse.lastIndexOf("]") + 2);
+		
+		// Using ObjectMapper, make a Page instance from string
 		ObjectMapper objectMapper = new ObjectMapper();
 		
 		Page page = null;
@@ -47,10 +49,30 @@ public class PathService {
 			e.printStackTrace();
 		}
 		
-		System.out.println("\n\nThis may have worked!");
-		System.out.println(page.getTitle());
-		System.out.println("\n\n and now for the real test");
-		System.out.println(page.getLinks());
+		// return page instance.
 		return page;
 	}
+	
+	public List<Path> generatePaths(String start, String end){
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		// create list of topics to return 
+		return Arrays.asList(
+				new Path(1, new ArrayList<String>(Arrays.asList("thisis my first url string", "this is my second url string", "put in from controller"))), 
+				new Path(4, new ArrayList<String>(Arrays.asList("second path url1", "secondpath url2"))), 
+				new Path(5, new ArrayList<String>(Arrays.asList("third path only has one")))
+				);
+	}
+
 }
