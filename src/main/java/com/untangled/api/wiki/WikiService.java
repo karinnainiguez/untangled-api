d25c6167dc29edda0eabc6f1f5f37b2d8b03f4a0
Karinna Iniguez <karinnainiguez@gmail.com>
2018-07-21 19:03:35 -0700

    Add helper methods for formatting name and making api call to wikipedia

--- src/main/java/com/untangled/api/wiki/WikiService.java
+++ src/main/java/com/untangled/api/wiki/WikiService.java
@@ -2,6 +2,7 @@ package com.untangled.api.wiki;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class WikiService {
@@ -9,12 +10,15 @@ public class WikiService {
 	@Autowired 
 	WikiRepository repo;
 	
+	RestTemplate restTemplate = new RestTemplate();
+	
 	public String practice() {
 		System.out.println("INSIDE THE METHOD");
 		System.out.println(repo.findAll());
 		
-//		repo.deleteAll();
 		connect("adding first", "second");
+		
+		apiCall("Ada Developers Academy");
 		
 		
 		
@@ -25,6 +29,22 @@ public class WikiService {
 	// HELPER CONNECT PARENT AND CHILD (creates wiki if not existant)
 	private void connect(String parent, String child) {
 		repo.addConnection(parent, child);
+	}
+	
+	// HELPER - call wikipedia api to get list of children
+	private void apiCall(String title) {
+		String stringResponse = restTemplate.getForObject(
+				"https://en.wikipedia.org/w/api.php?action=query&format=json&prop=links&pllimit=max&plnamespace=0&titles="
+						+ formatTitle(title),
+				String.class);
+		System.out.println(stringResponse);
+	}
+	
+	// HELPER - format titles
+	private String formatTitle(String title) {
+		String newTitle = title.replaceAll("%20", "_");
+		newTitle = newTitle.replaceAll("%20", "_");
+		return newTitle;
 	}
 
 }
